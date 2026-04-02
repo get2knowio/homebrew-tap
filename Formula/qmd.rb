ed7748b3e555b2d6b80dc0b204a3e77b67fb96d7
Paul O'Fallon <pofallon@users.noreply.github.com>
2026-04-02 07:26:50 -0400

    Add qmd formula (v2.0.1)

---
+++ Formula/qmd.rb
@@ -0,0 +1,21 @@
+class Qmd < Formula
+  include Language::Node
+
+  desc "Mini CLI search engine for docs, knowledge bases, and meeting notes"
+  homepage "https://github.com/tobi/qmd"
+  url "https://registry.npmjs.org/@tobilu/qmd/-/qmd-2.0.1.tgz"
+  sha256 "4f7156f50decebee8422f5e49e1b8b42db08b37b8e33446d83b28e391875a979"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/qmd --version")
+  end
+end
