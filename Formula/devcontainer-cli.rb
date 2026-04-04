a363d92b436782f81a92010a642e341fa579823e
Paul O'Fallon <505519+pofallon@users.noreply.github.com>
2026-04-04 01:05:46 +0000

    devcontainer-cli: add 0.85.0 bottle.

--- Formula/devcontainer-cli.rb
+++ Formula/devcontainer-cli.rb
@@ -9,6 +9,11 @@ class DevcontainerCli < Formula
   sha256 "54cb822bc2218186458e5690f67b0116f6800c45f5cb14671285e704a2ee2c29"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/get2knowio/homebrew-tap/releases/download/devcontainer-cli-0.85.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d690d6891ac25d91f2c9a1b54826c6559dee6325331608ab54507c2a2f50c403"
+  end
+
   depends_on "node"
 
   def install
